83ba0b6d9dec97ed645d03ed20055fa27e818865
DavidMcClelland-ux <davidDavid.McClelland@finance-ni.gov.uk>
2023-07-31 12:57:37 +0100

    update to ignore JS for navbar if not used in page

--- javascript/david-all.js
+++ javascript/david-all.js
@@ -1,16 +1,17 @@
-function initAll() {
-    var menuButton = document.querySelector(".david-navbutton");
-    var navMenu = document.querySelector(".david-navbar");
-    
-    
-    menuButton.addEventListener('click', function () {
-        
+function initAll () {
+    var menuButton = document.querySelector('.david-navbutton')
+    var navMenu = document.querySelector('.david-navbar')
+  
+    if (typeof menuButton !== 'undefined' && typeof navMenu !== 'undefined') {
+      menuButton.addEventListener('click', function () {
         if (navMenu.classList.contains('david-navbar--open')) {
-            this.setAttribute('aria-expanded', 'false');
-            navMenu.classList.remove('david-navbar--open');
+          this.setAttribute('aria-expanded', 'false')
+          navMenu.classList.remove('david-navbar--open')
         } else {
-            navMenu.classList.add('david-navbar--open');
-            this.setAttribute('aria-expanded', 'true');
+          navMenu.classList.add('david-navbar--open')
+          this.setAttribute('aria-expanded', 'true')
         }
-    });
-}
+      })
+    }
+  }
+  
